316306dad923e8bf878bd3ca1a096b10fc539fc7
Isaac Pinheiro <isaacpnhr@gmail.com>
2016-12-04 19:48:24 -0200

    Adicionando o struct da Mensagem.

--- server_cmd.h
+++ server_cmd.h
@@ -4,6 +4,12 @@
 #define MAX_PENDING 5
 #define BUFFER_SIZE 1024
 
+typedef struct {
+    char user_name[256];
+    char command[256];
+    char content[512];
+} Message;
+
 void *connection_handler(void *socket_desc);
 
 #endif
